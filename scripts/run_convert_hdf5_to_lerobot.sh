3f845a64e14e9fb840419a1057f66ce66cfc11a6
server <you@example.com>
2026-07-10 15:02:25 +0800

    chore(scripts): add hdf5 to lerobot conversion run script
    add a ready-to-use bash script to convert hdf5 dataset to lerobot format, set default dataset path and example parameters

---
+++ scripts/run_convert_hdf5_to_lerobot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export LEROBOT_HOME=/home/jszn/hewu/dataset
+
+python scripts/convert_new.py \
+  --raw_dir /home/jszn/hewu/dataset/1_put_the_rectangular_block_on_the_box \
+  --repo_id hewu2008/lerobot_put_rectangular_block_on_the_box
