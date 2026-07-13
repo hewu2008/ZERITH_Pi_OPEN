beb8ae84bb35c32db936dbb61cd4f1bff5ec144a
server <you@example.com>
2026-07-13 16:45:03 +0800

    fix(config): 修正模型权重加载的路径
    将权重加载路径补全为pi0_base/params，确保能正确找到模型参数文件

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -370,7 +370,7 @@ _CONFIGS = [
                 prompt_from_task=True,
             ),
         ),
-        weight_loader=weight_loaders.CheckpointWeightLoader("/home/jszn/hewu/model_zoo/pi0_base_params"),
+        weight_loader=weight_loaders.CheckpointWeightLoader("/home/jszn/hewu/model_zoo/pi0_base_params/pi0_base/params"),
         num_train_steps=30000,
         freeze_filter=pi0.Pi0Config(
             paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"
