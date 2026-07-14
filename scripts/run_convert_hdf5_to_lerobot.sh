65dda10b8057ffdcd48b5eb6d7d0ae1c1444fc56
server <you@example.com>
2026-07-14 16:18:46 +0800

    fix: 更新数据集相关配置到v2版本
    更新了转换脚本和训练配置中的数据集repo_id与asset_id，将其从pick_and_place_v1修改为pick_and_place_v2，同时调整了原始数据目录路径。

--- scripts/run_convert_hdf5_to_lerobot.sh
+++ scripts/run_convert_hdf5_to_lerobot.sh
@@ -3,5 +3,5 @@
 export LEROBOT_HOME=/home/jszn/hewu/dataset/
 
 python scripts/convert_new.py \
-  --raw_dir /home/jszn/hewu/dataset/1_put_the_rectangular_block_on_the_box \
-  --repo_id hewu2008/pick_and_place_v1
+  --raw_dir /home/jszn/hewu/dataset/1_put_the_part_in_the_box/ \
+  --repo_id hewu2008/pick_and_place_v2
