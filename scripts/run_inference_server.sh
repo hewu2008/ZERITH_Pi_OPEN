da0d75ca398681667ed21a53adbd6e959eb69c99
server <you@example.com>
2026-07-14 20:04:52 +0800

    chore: 更新推理脚本和训练配置的模型版本与路径
    将推理脚本中的模型 checkpoint 路径和数据集仓库id更新为pick_and_place_v2版本，同步修改训练配置中的对应配置项

--- scripts/run_inference_server.sh
+++ scripts/run_inference_server.sh
@@ -2,5 +2,5 @@
 
 python scripts/remote_infer_server.py \
   --policy.config=test \
-  --policy.dir=/home/jszn/hewu/alg-product/ZERITH_Pi_OPEN/openpi_checkpoints/test/pick_and_place_v1_2.5e-5_30k_bs8/22000 \
+  --policy.dir=/media/jszn/Data/hewu/openpi_checkpoints/test/pick_and_place_v2_2.5e-5_30k_bs8/2000 \
   --port=55555
