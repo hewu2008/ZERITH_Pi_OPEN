57ae0bda37001f24bf1f9c41fab5596f1f3d491c
server <you@example.com>
2026-07-28 17:54:12 +0800

    chore(runs): update training run scripts
    1. 新增pi05的训练启动脚本run_train_pi05.sh
    2. 修正pi0的训练脚本：修改实验名称并更新日志文件名

--- runs/run_train_pi0.sh
+++ runs/run_train_pi0.sh
@@ -8,5 +8,5 @@ mkdir -p "$LOG_DIR"
 
 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 \
 python scripts/train.py pi0_zerith \
-  --exp_name pick_and_place_v2_2.5e-5_30k_bs8 \
-  --overwrite 2>&1 | tee "$LOG_DIR"/"train_log.txt"
+  --exp_name pick_and_place_v2_2.5e-5_30k_bs8_pi0 \
+  --overwrite 2>&1 | tee "$LOG_DIR"/"train_log_pi0.txt"
---
+++ runs/run_train_pi05.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOG_DIR="logs"
+
+export LEROBOT_HOME=/home/jszn/hewu/dataset/
+
+mkdir -p "$LOG_DIR"
+
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 \
+python scripts/train.py pi05_zerith \
+  --exp_name pick_and_place_v2_2.5e-5_30k_bs8_pi05 \
+  --overwrite 2>&1 | tee "$LOG_DIR"/"train_log_pi05.txt"
