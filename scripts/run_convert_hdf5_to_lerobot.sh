bf754540c0bc05fdb487e67e23d447e5918d8db2
server <you@example.com>
2026-07-28 13:57:34 +0800

    chore(scripts): update hdf5 convert script's input path and repo id
    update the raw dataset directory and huggingface repo id for the conversion script

--- scripts/run_convert_hdf5_to_lerobot.sh
+++ scripts/run_convert_hdf5_to_lerobot.sh
@@ -3,5 +3,5 @@
 export LEROBOT_HOME=/home/jszn/hewu/dataset/
 
 python scripts/convert_new.py \
-  --raw_dir /home/jszn/hewu/dataset/1_put_the_rectangular_block_on_the_box_20260713 \
-  --repo_id hewu2008/pick_and_place_v1
+  --raw_dir /home/jszn/hewu/dataset/hdf5/1_clear_the_bin_box_20260720 \
+  --repo_id hewu2008/clear_bin_box_pi0
