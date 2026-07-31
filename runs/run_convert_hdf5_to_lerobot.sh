282bd96d62761e7e5c63fa4cbd507b43d6034c25
server <you@example.com>
2026-07-31 12:44:30 +0800

    chore: 更新hdf5转lerobot脚本的数据集路径和仓库id
    修改了转换脚本里的原始数据集目录和上传的huggingface仓库标识，适配v2版本的数据集

--- runs/run_convert_hdf5_to_lerobot.sh
+++ runs/run_convert_hdf5_to_lerobot.sh
@@ -3,5 +3,5 @@
 export LEROBOT_HOME=/home/jszn/hewu/dataset/
 
 python scripts/convert_new.py \
-  --raw_dir /home/jszn/hewu/dataset/hdf5/1_clear_the_bin_box_sanity \
-  --repo_id hewu2008/clear_bin_box_sanity
+  --raw_dir /home/jszn/hewu/dataset/hdf5/1_clear_the_bin_box_sanity_v2 \
+  --repo_id hewu2008/clear_bin_box_sanity_v2
