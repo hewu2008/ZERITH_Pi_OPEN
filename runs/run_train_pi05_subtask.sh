c70d6dd3c527d00d976b1c2e93ec6a95960c75d1
hewu2008 <hewu2008@gmail.com>
2026-08-14 08:46:50 +0800

    chore: 更新训练脚本的实验名称和评估数据集路径
    调整了实验名称的日期后缀为20260721，同时更新评估数据集路径匹配新的数据集版本

--- runs/run_train_pi05_subtask.sh
+++ runs/run_train_pi05_subtask.sh
@@ -11,7 +11,7 @@ mkdir -p "$LOG_DIR"
 
 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 \
 python scripts/train.py pi05_zerith_subtask \
-  --exp_name clear_the_bin_box_20260720_v2_5e-5_30k_bs16_ah30_pi05_full \
+  --exp_name clear_the_bin_box_20260721_v2_5e-5_30k_bs16_ah30_pi05_full \
   --eval_data_path /data/4T-1/hewu/dataset/hewu2008/clear_the_bin_box_20260721_v2 \
   --eval_traj_ids 0 1 2 3 \
   --eval_max_infer_time 40 \
