f25e0b4f7fcc1db0895aed315c428e01dc8237d8
hewu2008 <hewu2008@gmail.com>
2026-09-10 14:50:09 +0800

    chore: 更新hdf5转lerobot脚本的数据集路径和仓库id
    修改了LEROBOT_HOME环境变量路径、原始数据目录以及上传的huggingface仓库信息

--- runs/run_convert_hdf5_to_lerobot.sh
+++ runs/run_convert_hdf5_to_lerobot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export LEROBOT_HOME=/data/4T-1/hewu/dataset
+export LEROBOT_HOME=/data/4T-2/dataset
 
 python scripts/convert_new.py \
-  --raw_dir /data/4T-1/hewu/dataset/hdf5/clear_the_bin_box_20260720 \
-  --repo_id hewu2008/clear_the_bin_box_20260720
+  --raw_dir /data/4T-2/mocap_data/zerith/1_clear_the_bin_box \
+  --repo_id EmbodiedLab/clear_the_bin_box_20260910
