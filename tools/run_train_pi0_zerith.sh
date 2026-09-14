033dc4852581568fece329098840829c797f2d9e
hewu2008 <hewu2008@gmail.com>
2026-09-14 15:55:43 +0800

    chore(scripts): 修改训练脚本的项目名称为openpi_zerith
    将训练脚本里的project_name参数从openpi调整为openpi_zerith，以区分该零样本训练任务的项目标识

--- tools/run_train_pi0_zerith.sh
+++ tools/run_train_pi0_zerith.sh
@@ -5,7 +5,7 @@ export CUDA_VISIBLE_DEVICES=1
 export LEROBOT_HOME=/data/4T-2/dataset/
 
 python scripts/train.py pi0_zerith \
-  --project_name openpi \
+  --project_name openpi_zerith \
   --exp_name clear_the_bin_box_20260910_v1_60k_lr1e-5_bs8_ah50_pi0_full \
   --num_workers 4 \
   --overwrite 2>&1 | tee logs/train_log_pi0_full.log
